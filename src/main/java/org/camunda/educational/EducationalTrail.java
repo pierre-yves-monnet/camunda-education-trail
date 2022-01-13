9b19e914575a25ec37255cae3c64e310e4bb5630
Pierre-yves-monnet <pierre-yves.monnet@laposte.net>
2022-01-13 12:04:10 -0800

    Modification

---
+++ src/main/java/org/camunda/educational/EducationalTrail.java
@@ -0,0 +1,12 @@
+package org.camunda.educational;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationalTrail {
+    public static void main(String... args) {
+        SpringApplication.run(EducationalTrail.class, args);
+    }
+
+}
