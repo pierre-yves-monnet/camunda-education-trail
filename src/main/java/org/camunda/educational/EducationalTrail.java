6a26aa185ecbcead09f443dc4544052b81e58af3
Pierre-Yves <pierre-yves.monnet@laposte.net>
2022-08-15 16:09:11 -0700

    Add LDAP

--- src/main/java/org/camunda/educational/EducationalTrail.java
+++ src/main/java/org/camunda/educational/EducationalTrail.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableProcessApplication
+@EnableProcessApplication("connectidentitymanagement")
 
 public class EducationalTrail {
     public static void main(String... args) {
